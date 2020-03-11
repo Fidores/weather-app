5184d984cdd97862c8a76f12a37bf0ed2672578c
Fidores <45700014+Fidores@users.noreply.github.com>
2020-03-11 19:29:33 +0100

    Added hover effect to icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
-import { MainHeaderService } from './services/main-header/main-header.service';
+import { registerLocaleData } from '@angular/common';
+import localePl from '@angular/common/locales/pl';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
-import { registerLocaleData } from '@angular/common';
-import localePl from '@angular/common/locales/pl';
+import { MainHeaderService } from './services/main-header/main-header.service';
 
 @Component({
   selector: 'app-root',
@@ -12,7 +12,9 @@ import localePl from '@angular/common/locales/pl';
 })
 export class AppComponent implements OnInit {
   
-  constructor(private router: Router, private readonly header: MainHeaderService){}
+  constructor(
+    private readonly router: Router, 
+    private readonly header: MainHeaderService){}
 
   ngOnInit() {
     // Clean up header title after every route change. Its purpose is to reduce need for OnDestroy interface.
