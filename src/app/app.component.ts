5e8910edf6b10c03fbb292073867efa496c9dd2c
Fidores <45700014+Fidores@users.noreply.github.com>
2020-05-04 22:33:18 +0200

    Removed sprite injector

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,6 @@ import localePl from '@angular/common/locales/pl';
 import { Component, OnInit } from '@angular/core';
 import { concat } from 'rxjs';
 
-import { SpriteInjector } from './common/spriteInjector';
 import { AccountService } from './services/account/account.service';
 import { SettingsService } from './services/app-settings/settings.service';
 
@@ -11,13 +10,11 @@ import { SettingsService } from './services/app-settings/settings.service';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  providers: [{ provide: SpriteInjector, useClass: SpriteInjector }],
 })
 export class AppComponent implements OnInit {
   constructor(
     private readonly account: AccountService,
-    private readonly settings: SettingsService,
-    private readonly sprites: SpriteInjector
+    private readonly settings: SettingsService
   ) {}
 
   ngOnInit() {
@@ -27,8 +24,5 @@ export class AppComponent implements OnInit {
     if (this.account.isLoggedIn) {
       concat(this.settings.init(), this.account.loadUser()).subscribe();
     }
-
-    // Add weather icon pack as a sprite.
-    this.sprites.inject('../assets/icons/weather-icons/sprite.svg');
   }
 }
