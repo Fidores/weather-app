2b4b8b1f136dea8885ec1ef376d7ab8fb87b003a
Fidores <45700014+Fidores@users.noreply.github.com>
2020-05-24 23:03:42 +0200

    Fixed Fahrenheit units

--- src/app/pages/app-settings/app-settings.component.ts
+++ src/app/pages/app-settings/app-settings.component.ts
@@ -6,38 +6,40 @@ import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   templateUrl: './app-settings.component.html',
-  styleUrls: ['./app-settings.component.scss']
+  styleUrls: ['./app-settings.component.scss'],
 })
 export class AppSettingsComponent implements OnInit, OnDestroy {
-
-  constructor(
-    private readonly settings: SettingsService
-  ) { }
+  constructor(private readonly settings: SettingsService) {}
 
   subscriptions: Subscription = new Subscription();
 
   units: SelectOption[] = [
-    { value: 'imperal', name: 'Fahrenheit' },
+    { value: 'imperial', name: 'Fahrenheit' },
     { value: 'metric', name: 'Celsius' },
-    { value: '', name: 'Kelvin' }
-  ]
+    { value: '', name: 'Kelvin' },
+  ];
 
   settingsForm = new FormGroup({
-    units: new FormControl('metric')
+    units: new FormControl('metric'),
   });
 
   ngOnInit() {
     this.subscriptions.add(
-      this.settings.settings
-        .subscribe(appSettings => this.settingsForm.patchValue(appSettings, { emitEvent: false })));
+      this.settings.settings.subscribe(appSettings =>
+        this.settingsForm.patchValue(appSettings, { emitEvent: false })
+      )
+    );
 
-    this.subscriptions.add(this.settingsForm.valueChanges.subscribe(() => this.changeSettings()));
+    this.subscriptions.add(
+      this.settingsForm.valueChanges.subscribe(() => this.changeSettings())
+    );
   }
 
-  ngOnDestroy(){ this.subscriptions.unsubscribe() }
+  ngOnDestroy() {
+    this.subscriptions.unsubscribe();
+  }
 
   changeSettings() {
     this.settings.changeSettings(this.settingsForm.value).subscribe();
   }
-
 }
