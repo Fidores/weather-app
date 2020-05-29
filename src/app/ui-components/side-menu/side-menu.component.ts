ebf3d1ff0787ccee3e2434ea70f9e525f50311eb
Fidores <45700014+Fidores@users.noreply.github.com>
2020-05-29 22:45:02 +0200

    Hid some links when user is not logged in

--- src/app/ui-components/side-menu/side-menu.component.ts
+++ src/app/ui-components/side-menu/side-menu.component.ts
@@ -1,11 +1,22 @@
 import { User } from './../../models/User';
 import { AccountService } from './../../services/account/account.service';
-import { trigger, transition, state, style, animate } from '@angular/animations';
+import {
+  trigger,
+  transition,
+  state,
+  style,
+  animate,
+} from '@angular/animations';
 import { OverlayService } from './../../services/overlay/overlay.service';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { SideMenuService } from 'src/app/services/side-menu/side-menu.service';
 import { Subscription, Observable } from 'rxjs';
-import { faBars, faHome, faPlusSquare, faCog } from '@fortawesome/free-solid-svg-icons';
+import {
+  faBars,
+  faHome,
+  faPlusSquare,
+  faCog,
+} from '@fortawesome/free-solid-svg-icons';
 import { Router } from '@angular/router';
 
 @Component({
@@ -14,23 +25,20 @@ import { Router } from '@angular/router';
   styleUrls: ['./side-menu.component.scss'],
   animations: [
     trigger('sideMenuAnimation', [
-
       state('closed', style({ transform: 'translateX(-100%)' })),
       state('opened', style({ transform: 'translateX(0%)' })),
-      
-      transition('opened <=> closed', animate(200))
 
-    ])
-  ]
+      transition('opened <=> closed', animate(200)),
+    ]),
+  ],
 })
 export class SideMenuComponent implements OnInit, OnDestroy {
-
   constructor(
-    private readonly sideMenu: SideMenuService, 
+    private readonly sideMenu: SideMenuService,
     private readonly overlay: OverlayService,
     private readonly router: Router,
     private readonly account: AccountService
-  ) { }
+  ) {}
 
   isOpened: boolean = false;
   subscriptions: Subscription = new Subscription();
@@ -42,11 +50,25 @@ export class SideMenuComponent implements OnInit, OnDestroy {
   faCog = faCog;
 
   ngOnInit() {
-    this.subscriptions.add(this.sideMenu.closeMenuBroadcaster.subscribe(() => this.isOpened = false));
-    this.subscriptions.add(this.sideMenu.openMenuBroadcaster.subscribe(() => this.isOpened = true));
-    this.subscriptions.add(this.sideMenu.toggleMenuBroadcaster.subscribe(() => this.isOpened = !this.isOpened));
-    this.subscriptions.add(this.overlay.clickOnOverlayBroadcaster.subscribe(() => this.closeMenu()));
-    this.subscriptions.add(this.router.events.subscribe(() => this.closeMenu()));
+    this.subscriptions.add(
+      this.sideMenu.closeMenuBroadcaster.subscribe(
+        () => (this.isOpened = false)
+      )
+    );
+    this.subscriptions.add(
+      this.sideMenu.openMenuBroadcaster.subscribe(() => (this.isOpened = true))
+    );
+    this.subscriptions.add(
+      this.sideMenu.toggleMenuBroadcaster.subscribe(
+        () => (this.isOpened = !this.isOpened)
+      )
+    );
+    this.subscriptions.add(
+      this.overlay.clickOnOverlayBroadcaster.subscribe(() => this.closeMenu())
+    );
+    this.subscriptions.add(
+      this.router.events.subscribe(() => this.closeMenu())
+    );
   }
 
   ngOnDestroy() {
@@ -58,4 +80,7 @@ export class SideMenuComponent implements OnInit, OnDestroy {
     this.overlay.closeOverlay();
   }
 
+  isLoggedIn(): boolean {
+    return this.account.isLoggedIn;
+  }
 }
